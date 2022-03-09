9005b1e6fbce1733e68cea59205fadc444e4b818
bikashxsharma <bikashxsharma@gmail.com>
2022-03-09 20:17:02 +0200

    Head banner texts and medium link on footer

--- src/components/common/Footer/index.tsx
+++ src/components/common/Footer/index.tsx
@@ -1,36 +1,45 @@
-import React from 'react'
+import React from "react"
 
-import LinkedInIcon from '@material-ui/icons/LinkedIn'
-import GitHubIcon from '@material-ui/icons/GitHub'
+import LinkedInIcon from "@material-ui/icons/LinkedIn"
+import GitHubIcon from "@material-ui/icons/GitHub"
 
-import './style.scss'
+import "./style.scss"
 
 const Footer = () => {
 	return (
-		<div className='footer'>
-			<div className='footer__container'>
+		<div className="footer">
+			<div className="footer__container">
 				<h4>Follow me on social media</h4>
-				<div className='footer__social-icons'>
+				<div className="footer__social-icons">
 					<a
-						href='https://github.com/bikashxsharma'
-						rel='noopener noreferrer'>
-						{' '}
-						<GitHubIcon className='social-icon' />
+						href="https://github.com/bikashxsharma"
+						target="_blank"
+						rel="noopener noreferrer">
+						{" "}
+						<GitHubIcon className="social-icon" />
 					</a>
 					<a
-						href='https://www.linkedin.com/in/bikashxsharma/'
-						rel='noopener noreferrer'>
-						<LinkedInIcon className='social-icon' />
+						href="https://www.linkedin.com/in/bikashxsharma/"
+						target="_blank"
+						rel="noopener noreferrer">
+						<LinkedInIcon className="social-icon" />
 					</a>
 					<a
-						href='https://www.behance.net/user/?username=bikashxsharma'
-						rel='noopener noreferrer'>
-						Be
+						href="https://www.behance.net/user/?username=bikashxsharma"
+						target="_blank"
+						rel="noopener noreferrer">
+						<span className="social-icon">Be</span>
+					</a>
+					<a
+						href="https://bikashxsharma.medium.com/"
+						target="_blank"
+						rel="noopener noreferrer">
+						<span style={{fontSize: "20px"}}>Medium</span>
 					</a>
 				</div>
 				<p>
-					Designed and developed by Bikash Sharma. Built
-					with React. Hosted on Netlify.
+					Designed and developed by Bikash Sharma. Built with React. Hosted on
+					Netlify.
 				</p>
 				<p>Copyright © Bikash {new Date().getFullYear()}</p>
 			</div>
--- src/components/landing/Header/Banner/index.tsx
+++ src/components/landing/Header/Banner/index.tsx
@@ -1,76 +1,70 @@
-import React from 'react'
+import React from "react"
 
-import HeroImage from '../../../../assets/images/hero-image.png'
-import HeaderBg from '../../../../assets/images/header__bg.png'
-import Button from '../../../common/Button'
+import HeroImage from "../../../../assets/images/hero-image.png"
+import HeaderBg from "../../../../assets/images/header__bg.png"
+import Button from "../../../common/Button"
 
-import './style.scss'
+import "./style.scss"
 
 export const Banner = () => {
 	return (
-		<div className='banner'>
-			<div className='banner__container'>
-				<div className='banner__content'>
+		<div className="banner">
+			<div className="banner__container">
+				<div className="banner__content">
 					<h3>HELLO, I’M BIKASH SHARMA</h3>
 					<h1>Full Stack Developer</h1>
 					<h5>
-						I’m a Helsinki based full stack developer
-						focused on writing code that makes an{' '}
-						<span className='text-highlight'>
-							impact to users.
-						</span>
-						 I have also gained experience as a consultant
-						,
-						<span className='text-highlight'>
-							product designer
-						</span>
-						, and digital marketer. Currently a co-founder and Lead Product Developer at <a
-							className='github'
-							href='https://wonsta.io'
-							target='_blank'
-							rel='noopener noreferrer'>
+						I’m a Helsinki-based full-stack developer focused on writing code
+						that makes an{" "}
+						<span className="text-highlight">impact on users.</span> I have also
+						gained experience as a consultant,
+						<span className="text-highlight"> product designer</span>, and
+						digital marketer. Currently I am a co-founder and the Lead Product
+						Developer at{" "}
+						<a
+							className="github"
+							href="https://wonsta.io"
+							target="_blank"
+							rel="noopener noreferrer">
 							Wonsta.
-						</a>. I commit code
-						to{' '}
+						</a>{" "}
+						I commit code to{" "}
 						<a
-							className='github'
-							href='https://github.com/bikashxsharma'
-							target='_blank'
-							rel='noopener noreferrer'>
+							className="github"
+							href="https://github.com/bikashxsharma"
+							target="_blank"
+							rel="noopener noreferrer">
 							Github.
-						</a>Make videos on{' '}
+						</a>{" "}
+						Make videos on{" "}
 						<a
-							className='github'
-							href='https://www.youtube.com/channel/UCKtcuEBjG-8T1HDrpuAprww/videos'
-							target='_blank'
-							rel='noopener noreferrer'>
-							 Bikashweb.
-						</a>{' '}And write articles on <a
-							className='github'
-							href='https://bikashxsharma.medium.com/'
-							target='_blank'
-							rel='noopener noreferrer'>
+							className="github"
+							href="https://www.youtube.com/channel/UCKtcuEBjG-8T1HDrpuAprww/videos"
+							target="_blank"
+							rel="noopener noreferrer">
+							Bikashweb.
+						</a>{" "}
+						And write articles on{" "}
+						<a
+							className="github"
+							href="https://bikashxsharma.medium.com/"
+							target="_blank"
+							rel="noopener noreferrer">
 							Medium.
 						</a>
 					</h5>
-					<a href='#recent-projects'>
-						<Button
-							label='View projects'
-							variant='outline'
-						/>
+					<a href="#recent-projects">
+						<Button label="View projects" variant="outline" />
 					</a>
 				</div>
-				<div className='banner__image'>
-					<img
-						src={HeroImage}
-						alt='Bikash Sharma smiling'
-					/>
+				<div className="banner__image">
+					<img src={HeroImage} alt="Bikash Sharma smiling" />
 				</div>
 			</div>
 			<img
 				src={HeaderBg}
-				alt='code abstract background'
-				className='banner__bg'
+				alt="code abstract background"
+				className="banner__bg"
 			/>
 		</div>
 	)
