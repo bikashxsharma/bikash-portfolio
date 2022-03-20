d96e8ed88c03f6876dedf0e6e28eab3d39a013fe
bikashxsharma <bikashxsharma@gmail.com>
2022-03-20 20:52:46 +0200

    React GA modified with new stream

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,21 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import {BrowserRouter} from 'react-router-dom'
-import * as serviceWorker from './serviceWorker'
-import ReactGA from 'react-ga'
+import React from "react"
+import ReactDOM from "react-dom"
+import {BrowserRouter} from "react-router-dom"
+import * as serviceWorker from "./serviceWorker"
+import ReactGA from "react-ga"
 
+import "./index.css"
+import App from "./App"
 
-import './index.css'
-import App from './App'
-
-export const initGA = () => {
-	ReactGA.initialize('UA-179274475-1')
-}
-export const GApageView = (
-	page: string,
-) => {
-	ReactGA.pageview(page)
-}
-export const GAmodalView = (
-	modal: string,
-) => {
-	ReactGA.modalview(modal)
-}
+const TRACKING_ID = "G-5X4R9D4GWT"
+ReactGA.initialize(TRACKING_ID)
+ReactGA.pageview(window.location.pathname)
 
 ReactDOM.render(
 	<BrowserRouter>
 		<App />
 	</BrowserRouter>,
-	document.getElementById('root'),
+	document.getElementById("root"),
 )
 
 // If you want your app to work offline and load faster, you can change
