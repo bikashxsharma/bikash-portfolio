c487e4aeea00fa9b5ff7664d734e9226a3eb7bbb
bikashxsharma <bikashxsharma@gmail.com>
2022-03-21 19:57:20 +0200

    Google analytics in head

--- src/components/common/ContactForm/index.tsx
+++ src/components/common/ContactForm/index.tsx
@@ -1,47 +1,34 @@
-import React from 'react'
-import { ReactTypeformEmbed } from 'react-typeform-embed'
-import HighlightOffIcon from '@material-ui/icons/HighlightOff'
+import React from "react"
+import {ReactTypeformEmbed} from "react-typeform-embed"
+import HighlightOffIcon from "@material-ui/icons/HighlightOff"
 
-import { GAmodalView } from 'index'
-
-import './style.scss'
+import "./style.scss"
 
 type FormProp = {
 	onclose: Function
 }
 
 const styleDefault = {
-	position: 'relative',
-	width: '100%',
-	height: '100%',
-	overflow: 'hidden',
+	position: "relative",
+	width: "100%",
+	height: "100%",
+	overflow: "hidden",
 }
-const ContactForm: React.FC<FormProp> = ({
-	onclose,
-}) => {
+const ContactForm: React.FC<FormProp> = ({onclose}) => {
 	const closeModal = () => {
 		onclose(false)
 	}
-	React.useEffect(() => {
-		GAmodalView('Contact popup')
-	}, [])
+
 	return (
-		<div
-			onClick={closeModal}
-			className='contact-form'>
-			<div className='form__inner'>
-				<HighlightOffIcon
-					className='form__close'
-					onClick={closeModal}
-				/>{' '}
+		<div onClick={closeModal} className="contact-form">
+			<div className="form__inner">
+				<HighlightOffIcon className="form__close" onClick={closeModal} />{" "}
 				<ReactTypeformEmbed
-					url='https://bikashsharma.typeform.com/to/XCm2PQqB'
+					url="https://bikashsharma.typeform.com/to/XCm2PQqB"
 					popup={false}
 					style={styleDefault}
-				/>{' '}
-				<h3 className='form__loader'>
-					loading..
-				</h3>
+				/>{" "}
+				<h3 className="form__loader">loading..</h3>
 			</div>
 		</div>
 	)
--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -1,21 +1,17 @@
-import React from 'react'
+import React from "react"
 
-import Header from 'components/landing/Header/Header'
-import Skills from 'components/landing/Skills'
-import RecentProjects from 'components/landing/RecentProjects'
-import ClientProjects from 'components/landing/ClientProjects'
-import ArticlesVideos from 'components/landing/ArticlesVideos'
-import Experience from 'components/landing/Experience'
-import CtaCard from 'components/common/CtaCard'
-import Footer from 'components/common/Footer'
-import { initGA } from 'index'
+import Header from "components/landing/Header/Header"
+import Skills from "components/landing/Skills"
+import RecentProjects from "components/landing/RecentProjects"
+import ClientProjects from "components/landing/ClientProjects"
+import ArticlesVideos from "components/landing/ArticlesVideos"
+import Experience from "components/landing/Experience"
+import CtaCard from "components/common/CtaCard"
+import Footer from "components/common/Footer"
 
-import './style.scss'
+import "./style.scss"
 
 const Home = () => {
-	React.useEffect(() => {
-		initGA()
-	}, [])
 	return (
 		<div>
 			<Header />
@@ -23,19 +19,19 @@ const Home = () => {
 			<RecentProjects />
 			<ClientProjects />
 			<CtaCard
-				title='Interested in Collaboration?'
-				detail='Let’s have a conversation! I’d love to hear about
+				title="Interested in Collaboration?"
+				detail="Let’s have a conversation! I’d love to hear about
 					what you’re working on and find a way to work
-					together.'
-				btVariant='solid'
-				label='Get in touch'
+					together."
+				btVariant="solid"
+				label="Get in touch"
 			/>
 			<Experience />
 			<ArticlesVideos />
 			<CtaCard
-				title='Interested in Collaboration?'
-				btVariant='solid'
-				label='Get in touch'
+				title="Interested in Collaboration?"
+				btVariant="solid"
+				label="Get in touch"
 			/>
 			<Footer />
 		</div>
